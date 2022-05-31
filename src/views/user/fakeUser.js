b435e6a4855ce8861712aef17beb78a9214f5a83
Aoanhcms <mr.hanleve@gmail.com>
2022-05-31 15:48:47 +0700

    fakeUser New Line

--- src/views/user/fakeUser.js
+++ src/views/user/fakeUser.js
@@ -14,4 +14,22 @@ export default [{
   birthday: '09-12-2002',
   created_at: '09-12-2002',
   updated_at: '09-12-2002',
-}]
+},
+{
+  id: 2,
+  company: 'AAAA',
+  customer: 'Sinh danh',
+  phone_number: '096998555',
+  country: 'VN',
+  status: 'ON',
+  groups: [{
+    id: 1,
+    name: 'group',
+  }],
+  creater: 'Sinh danh',
+  gender: 'male',
+  birthday: '09-12-2002',
+  created_at: '09-12-2002',
+  updated_at: '09-12-2002',
+},
+]
